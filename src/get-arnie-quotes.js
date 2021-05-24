97d1ae45891a99f4665c6205ae6d2218f205787f
Yifan Ai <yifanai@aol.com>
2021-05-24 23:41:39 +1000

    Finish implementing getArnieQuotes()

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,23 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const getArnieQuote = async (url) => {
+  // execute a mock HTTP GET request
+  const { body, status } = await httpGet(url);
+
+  // create an object to contain the response message
+  const key = status === 200 ? 'Arnie Quote' : 'FAILURE';
+  const value = JSON.parse(body).message;
+  return {
+    [key]: value
+  };
 };
 
-module.exports = {
-  getArnieQuotes,
+const getArnieQuotes = async (urls) => {
+  // send the requests
+  const requests = urls.map((url) => getArnieQuote(url));
+
+  // return the promise
+  return await Promise.all(requests);
 };
+
+module.exports = { getArnieQuotes };
